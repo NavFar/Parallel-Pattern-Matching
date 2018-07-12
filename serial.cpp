32c6318435312976f263f5e1f18030c99bb15afa
Navid Farahmand <NavidFarahmand75@gmail.com>
2018-07-12 20:39:59 +0430

    add rotate search

--- serial.cpp
+++ serial.cpp
@@ -16,12 +16,12 @@ int main( int argc, char* argv[] )
                 BMP pattern,image;
                 image.ReadFromFile(argv[i]);
                 pattern.ReadFromFile(argv[i+1]);
-                vector<uint>res;
+                vector<uint>res1,res2;
                 auto start_time = chrono::high_resolution_clock::now();
-                patternMatching(image,pattern,res);
+                patternMatching(image,pattern,res1);
                 auto end_time = chrono::high_resolution_clock::now();
                 auto totalTime = chrono::duration_cast<chrono::seconds>(end_time - start_time);
-                cout<<res.size()/2<<"times.\n";
+                cout<<(res1.size()+res2.size())/2<<"times.\n";
                 cout<<totalTime.count()<<"Seconds.\n";
         }
         return 0;
@@ -53,6 +53,7 @@ void patternMatching(BMP &image,BMP& pattern,vector<uint>& res){
                                      (image(i,j)->Blue*image(i,j)->Blue))/3;
                 }
         }
+        vector<uint> tempRes1;
         double maxValue=0;
         bool flag=1;
         for(int i=0; i<image.TellWidth()-pattern.TellWidth(); i++)
@@ -75,15 +76,55 @@ void patternMatching(BMP &image,BMP& pattern,vector<uint>& res){
                         if(flag||(curVal>maxValue))
                         {
                                 flag=0;
-                                res.clear();
-                                res.push_back(i);
-                                res.push_back(j);
+                                tempRes1.clear();
+                                tempRes1.push_back(i);
+                                tempRes1.push_back(j);
                                 maxValue=curVal;
                         }else if(curVal == maxValue) {
-                                res.push_back(i);
-                                res.push_back(j);
+                                tempRes1.push_back(i);
+                                tempRes1.push_back(j);
                         }
                 }
         }
+
+        vector<uint> tempRes2;
+        BMP RPattern;
+        rotateImage(pattern, RPattern);
+        maxValue=0;
+        flag=1;
+        for(int i=0; i<image.TellWidth()-RPattern.TellWidth(); i++)
+        {
+                for(int j=0; j<image.TellHeight()-RPattern.TellHeight(); j++)
+                {
+                        double curVal = 0;
+                        long ISum=0;
+                        for(int k=0; k<RPattern.TellWidth(); k++)
+                        {
+                                for(int z=0; z<RPattern.TellHeight(); z++)
+                                {
+                                        curVal+=((RPattern(k,z)->Red*image(i+k,j+z)->Red)+
+                                                 (RPattern(k,z)->Green*image(i+k,j+z)->Green)+
+                                                 (RPattern(k,z)->Blue*image(i+k,j+z)->Blue))/3;
+                                        ISum+=IMult[i+k][j+z];
+                                }
+                        }
+                        curVal=(curVal*curVal)/(ISum*TSum);
+                        if(flag||(curVal>maxValue))
+                        {
+                                flag=0;
+                                tempRes2.clear();
+                                tempRes2.push_back(i);
+                                tempRes2.push_back(j);
+                                maxValue=curVal;
+                        }else if(curVal == maxValue) {
+                                tempRes2.push_back(i);
+                                tempRes2.push_back(j);
+                        }
+                }
+        }
+        for(uint i=0; i<tempRes1.size(); i++)
+                res.push_back(tempRes1.at(i));
+        for(uint i=0; i<tempRes2.size(); i++)
+                res.push_back(tempRes2.at(i));
         return;
 }
